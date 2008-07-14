e34b06d981864605e4c49452fd632b29830e6f03
kendowns <kendowns@d2947088-3ffd-11de-917f-4137a9cc17b9>
2008-07-14 13:18:21 +0000

    Added a vastly simplified Template that looks like rt_pixel but is far easier to work with.
    1) Supports 4 color schemes from rt_pixel
    2) Vastly simplified menu building
    3) Supports 3 monitor resolutions: 800x600, 1024x768 and 1400x1050
    4) Simplified style sheets, much easier to add new color schemes
    5) Standard styles like "dark" "light", "darkgray" and "lightgray" that give template standardized-colors to generated HTML.

---
+++ templates/pixel2/index.php
@@ -0,0 +1,109 @@
+<?php defined( '_VALID_MOS' ) 
+    or die( 'Direct Access to this location is not allowed.' );
+?>
+<?php 
+# ========================================================
+# The templating system expects these functions
+# ========================================================
+function tmpCountModules($module) { return true; }
+function tmpLoadModules($module) {
+    if($module=='footer') {
+        echo configGet('notesfooter');
+    }
+}
+function loadMenu() {
+    if (LoggedIn()) {
+        $dir = dirname(__FILE__);
+        include $dir."/pixel2menu.php";        
+    }
+}
+# ========================================================
+# Get some cookie stuff taken care of before header
+# ========================================================
+$template = $mainframe->getTemplate();
+
+$app   = $GLOBALS['AG']['application'];
+if(gpExists('p2c')) {
+    $color = gp('p2c');
+    setCookie($app."_color",hx($color),strtotime("+5 years",time()));
+}
+else {
+    $color = a($_REQUEST,$app."_color",'blue');
+}
+if(gpExists('p2s')) {
+    $size  = gp('p2s');
+    setCookie($app."_size",hx($size),strtotime("+5 years",time()));
+}
+else {
+    $size  = a($_REQUEST,$app."_size",'1024');
+}
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php
+
+cssInclude("templates/$template/css/pixel2.css");
+cssInclude("templates/$template/css/pixel2-$size.css");
+cssInclude("templates/$template/css/pixel2-$color.css");
+include('androHTMLHead.php');
+?>
+</head>
+<body>
+<center>
+    <div class="template" style="text-align: left">
+        <?php if (mosCountModules('header')) { ?>
+            <div class="templateheader"><?=mosLoadModules('header')?>
+            <div class="templateheader-top">
+                <img src="templates/<?=$template?>/images/logo.gif">
+                <div class='sizepicker'>
+                <a href="?p2s=800">800</a>&nbsp;
+                <a href="?p2s=1024">1024</a>&nbsp;
+                <a href="?p2s=1400">1400</a>&nbsp;
+                </div>
+                <div class='colorpicker'>
+                <a href="?p2c=blue"   class="cp_blue">&nbsp;&nbsp;</a>&nbsp;&nbsp;
+                <a href="?p2c=orange" class="cp_orange">&nbsp;&nbsp;</a>&nbsp;&nbsp;
+                <a href="?p2c=green"  class="cp_green">&nbsp;&nbsp;</a>&nbsp;&nbsp;
+                <a href="?p2c=red"    class="cp_red">&nbsp;&nbsp;</a>&nbsp;&nbsp;
+                <a href="?p2c=gs"   class="cp_gs">&nbsp;&nbsp;</a>&nbsp;&nbsp;
+                </div>
+            </div>
+            </div>
+        <?php } ?>
+        <?php if (mosCountModules('menu')) { ?>
+            <div class="templatemenu"><?=loadMenu()?>
+                <?=fwModuleMenuRight()?>
+            </div>
+            <div style="clear: both"></div>
+        <?php } ?>
+        <?php if (mosCountModules('shortcuts')) { ?>
+            <div class="templateshortcuts"><?=mosLoadModules('shortcuts')?>
+            </div>
+        <?php } ?>
+    
+        <!-- the explicit cellspacing is required for IE -->
+        <table class="template" cellspacing=0 cellpadding=0>
+            <tr>
+                <?php if (mosCountModules('left')) { ?>
+                    <td class="auxiliary templateleft">
+                        <?=mosLoadModules('left')?>
+                    </td>
+                <?php } ?>
+            
+                <td class="templatemain">
+                <?=mosMainBody()?>
+                </td>
+            </tr>
+        </table>        
+    
+        <?php if (mosCountModules('footer')) { ?>
+            <div class="templatefooter"><?=mosLoadModules('footer')?>
+            </div>
+        <?php } ?>
+    </div>
+</center>
+<?php include('androHTMLFoot.php'); ?>
+</body>
+</html>
---
+++ templates/pixel2/pixel2menu.php
@@ -0,0 +1,97 @@
+<?php
+# ==================================================================
+# DO NOT ALLOW DIRECT ACCESS
+# ==================================================================
+defined( '_VALID_MOS' ) 
+    or die( 'Direct Access to this location is not allowed.' );
+
+# ==================================================================
+# AREA 1: Render the menu.  Note we directly output it
+# ==================================================================
+$menus=SessionGET("AGMENU");
+$ulpads = html('ul');
+$first = 0;
+foreach($menus as $menuid=>$menuinfo) {
+    $pad = $ulpads->h('li');
+    $span= $pad->h('span',$menuinfo['description']);
+    $span->hp['onmouseover'] = "pixel2ShowMenu('$menuid')";
+    $span->hp['onmouseout']  = "window.pixel2pad=false;pixel2HideMenu('$menuid')";
+    $span->hp['id'] = 'pixel2_pad_'.$menuid;
+
+    $ul  = $pad->h('ul');
+    $ul->hp['id'] = 'pixel2_menu_'.$menuid;
+    $ul->addClass('dropdown');
+    $ul->hp['style'] = 'display:none;';
+    $ul->hp['onmouseover'] = "pixel2ShowMenu('$menuid')";
+    $ul->hp['onmouseout']  = "window.pixel2pad=false;pixel2HideMenu('$menuid')";
+    foreach($menuinfo['items'] as $page=>$pageinfo) {
+        $pd = $pageinfo['description'];
+        $li = $ul->h('li');
+        #$li->hp['onmouseover'] = "pixel2ShowMenu('$menuid')";
+        #$li->hp['onmouseout']  = "window.pixel2pad=false;pixel2HideMenu('$menuid')";
+        $a  = html('a',$li,$pd);
+        if($pageinfo['uix2'] == 'Y') {
+            $a->hp['href'] = "?gp_page=$page&x2=1";
+        }
+        else {
+            $a->hp['href'] = "?x4Page=$page";
+        }
+    }
+}
+
+#$li = $ulpads->html('li');
+#$a  = $li->a('Logout','?st2logout=1');
+#$a->hp['style'] = 'color: white; border-width: 0';
+
+$ulpads->render();
+
+# ==================================================================
+# AREA 2: Some script
+# ==================================================================
+?>
+<script>
+pixel2pad = false;
+function pixel2ShowMenu(menuid) {
+    var obj = $a.byId('pixel2_menu_'+menuid);
+    $('#pixel2_pad_'+menuid).addClass('selected');
+    if($a.p(obj,'zHeight','')=='') {
+        obj.style.opacity=0;
+        obj.style.display='block';
+        obj.zHeight = $(obj).height();
+        obj.style.height = '0px';
+        obj.style.opacity = 1;
+    }
+    if(window.pixel2pad!=menuid) {
+        // mark this as current and slide it down
+        window.pixel2pad = menuid;
+        $(obj)
+            .stop()
+            .css('opacity',1)
+            .animate({height: obj.zHeight},pixel2Speed(obj,true));
+    }
+}
+
+function pixel2HideMenu(menuid) {
+    setTimeout(function() {
+        if(window.pixel2pad != menuid) {
+            $('#pixel2_pad_'+menuid).removeClass('selected');
+            var obj = $a.byId('pixel2_menu_'+menuid);
+            $(obj).stop().animate(
+                {height: 0}
+                ,pixel2Speed(obj,false)
+                ,null
+                ,function() {this.style.opacity=0}
+             );
+        }
+    },300);
+}
+
+function pixel2Speed(obj,goDown) {
+    if(goDown) 
+        var distance = obj.zHeight - $(obj).height();
+    else
+        var distance = $(obj).height();
+        
+    return distance;        
+}
+</script>
