bd5615fbb89d30a967d61ed870beb6913f5bde3e
kendowns <kendowns@d2947088-3ffd-11de-917f-4137a9cc17b9>
2008-12-01 12:37:00 +0000

    Eliminated earlier use of 'x6plugin' concept and moved that code into basic HTML generation.  Modified androX6 so the "profiles" no longer call the plugins, they just call basic HTML generation.

--- lib/androX6.php
+++ lib/androX6.php
@@ -2,7 +2,7 @@
 /****c* PHP-API/androX6
 *
 * NAME
-*   androX4
+*   androX6
 *
 * FUNCTION
 *   The PHP class androX6 is the base class for all Andromeda
@@ -63,9 +63,9 @@ class androX6 {
 
         SQL("set datestyle to US");
         SQL("set datestyle to SQL");
-        $skey = SQLFN(gp('x4w_skey'));
+        $skey = SQLFN(gp('x6w_skey'));
         $row = SQL_OneRow("SELECT * FROM $view WHERE skey=$skey");
-        x4Data('row',$row);
+        x6Data('row',$row);
     }
 
     # ===================================================================
@@ -77,8 +77,8 @@ class androX6 {
         $table_id = gp('x6page');
         $dd = ddTable($table_id);
         
-        $row0 = aFromGP('x4v_');
-        $row1 = aFromgp('x4inp_'.$table_id.'_');
+        $row0 = aFromGP('x6v_');
+        $row1 = aFromgp('x6inp_'.$table_id.'_');
         $row = array_merge($row0,$row1);
         if(a($row,'skey',0)==0) unset($row['skey']);
         
@@ -122,7 +122,7 @@ class androX6 {
                 if(trim($value)=='') continue;
                 
                 if(strpos($value,'/')===false && strpos($value,'-')===false) {
-                    x4Error($ermsg);
+                    x6Error($ermsg);
                     $errors = true;
                     continue;
                 }
@@ -130,11 +130,11 @@ class androX6 {
                     $parsed = explode('/',$value);
                     if(count($parsed)<>3) {
                         $errors = true;
-                        x4Error($ermsg);
+                        x6Error($ermsg);
                         continue;
                     }
                     if(!checkdate($parsed[0],$parsed[1],$parsed[2])) {
-                        x4Error($ermsg2);
+                        x6Error($ermsg2);
                         $errors = true;
                         continue;
                     }
@@ -143,11 +143,11 @@ class androX6 {
                     $parsed = explode('-',$value);
                     if(count($parsed)<>3) {
                         $errors = true;
-                        x4Error($ermsg);
+                        x6Error($ermsg);
                         continue;
                     }
                     if(!checkdate($parsed[1],$parsed[2],$parsed[0])) {
-                        x4Error($ermsg2);
+                        x6Error($ermsg2);
                         $errors = true;
                         continue;
                     }
@@ -163,7 +163,7 @@ class androX6 {
                     "Select * FROM {$dd['viewname']} WHERE skey = $skey"
                 );
             }
-            x4Data('row',$row);
+            x6Data('row',$row);
         }
         else {
             SQLX_Update($dd,$row);
@@ -172,8 +172,8 @@ class androX6 {
                 $row=SQL_OneRow(
                     "Select * FROM {$dd['viewname']} WHERE skey = $skey"
                 );
-                x4Debug($row);
-                x4Data('row',$row);
+                x6Debug($row);
+                x6Data('row',$row);
             }
         }
     }
@@ -192,7 +192,7 @@ class androX6 {
         $skey = SQLFN(gp('skey'));
         $sq="Delete from $view where skey = $skey";
         if(Errors()) {
-            x4Errors(hErrors());
+            x6Errors(hErrors());
         }
         SQL($sq);
     }
@@ -226,7 +226,7 @@ class androX6 {
             ."  FROM ".ddTable_idResolve($table_id_fko)
             ." WHERE ".$this->dd['fk_parents'][$table_id_fko]['cols_par']."= $value";
         $answer = SQL_OneRow($sql);
-        x4Data('fetch',$answer);
+        x6Data('fetch',$answer);
     }
 
     # ===================================================================
@@ -235,6 +235,8 @@ class androX6 {
     #
     # ===================================================================
     function browseFetch() {
+        $table_id = $this->dd['table_id'];
+        
         #  This is the list of columns to return 
         $acols = explode(',',$this->dd['projections']['_uisearch']);
 
@@ -288,6 +290,7 @@ class androX6 {
         #        
         $ascDesc = gp('sortAD')=='ASC' ? ' ASC' : ' DESC';
         $aorder = array();
+        $searchsort = '';
         if(gpExists('sortAsc')) {
             x6Debug(gp('sortAsc'));            
             $ascDesc = gp('sortAsc')=='true' ? ' ASC' : ' DESC';
@@ -355,7 +358,8 @@ class androX6 {
         $gridHeight=gp('xGridHeight',500);
         
         # Now make up the generic div and add all of the cells
-        $grid = new androHTMLTabDiv($gridHeight,false,$sortable);
+        $bb = gp('xButtonBar','N')=='Y';
+        $grid = new androHTMLTabDiv($gridHeight,$table_id,false,$sortable,$bb);
         $this->tabDivGeneric($grid,$this->dd,$tabPar);
         $grid->addData($answer);
         
@@ -377,21 +381,183 @@ class androX6 {
             x6html('*MAIN*',ob_get_clean());
         }
         
-        # ..otherwise just send the body back
+        # ..otherwise just send the body back.  But kill
+        #   any script they created.
+        x6ScriptKill();
         x6Html('browseFetchHtml',$grid->dbody->bufferedRender());
         return;
-        ob_start();
-        foreach($grid->dbody->children as $child) {
-            $child->render();
-        }
-        x6Html('browseFetchHtml',ob_get_clean());
-        return;
     }
+    # ===================================================================
+    # *******************************************************************
+    #
+    # Profile 0: "tabDiv"  editable!
+    #
+    # *******************************************************************
+    # ===================================================================
+    function profile_tabDiv() {
+        # these were provided by the code that instantiated
+        # and initialized the object.
+        $dd       = $this->dd;
+        $table_id = $this->dd['table_id'];
+        
+        # Scan through and look for a single queuepos column,
+        # it changes things considerably: the queuepos column
+        # is hidden and sorting is forced on that column.  It is
+        # also assigned automatically as the user creates rows.
+        $queuepos = '';
+        foreach($dd['flat'] as $colname=>$colinfo) {
+            if(strtolower($colinfo['automation_id'])=='queuepos') {
+                $queuepos = $colname;
+                break;
+            }
+        }
+        # Make sortable or not based on queuepos
+        $sortable = $queuepos=='';
+        
+        # Create the top level div
+        $top=new androHTMLTableController($table_id);
+        $top->addClass('fadein');
+        
+        $hremain = x6cssDefine('insideheight');
+        
+        # Get us the basic title
+        $top->h('h1',$dd['description']);
+        $hremain -= x6cssHeight('h1');
+        $hremain -= x6cssHeight('div.x6buttonBar a.button');
+        $clearboth = $top->h('div');
+        $clearboth->hp['style'] = 'clear: both';
+        
+        # Work out a height by finding out inside height
+        # and subtracing line height a few times
+        $gridHeight = $hremain - x6cssHeight('h1');
+        $grid       = $top->addTabDiv(
+            $gridHeight,$table_id,false,$sortable,true,true
+        );
+        $grid->hp['x6profile'] = 'x6tabDiv';
+        
+        # More features specific to this profile, these will 
+        # allow browseFetch not to have to figure this all out
+        # all over again.
+        $grid->ap['xGridHeight'] = $gridHeight;
+        $grid->ap['xSortable']   = $sortable ? "Y" : "N";
+        $grid->ap['xReturnAll']  = "Y";
+
+        # If queuepos set, set this flag        
+        if($queuepos) $grid->ap['xInsertAfter'] = 'Y';
+        
+        # Now obtain the _uisearch columns and make one column each
+        $uisearch = $dd['projections']['_uisearch'];
+        $aColumns = explode(',',$uisearch);
+        foreach($aColumns as $idx=>$column) {
+            if($column==$queuepos) unset($aColumns[$idx]);
+        }
+        foreach($aColumns as $column) {
+            $desc = $dd['flat'][$column]['descshort'];
+            if($desc=='') {
+                $desc = $dd['flat'][$column]['description'];
+            }
+            $options=array(
+                'column_id' =>  $column
+                ,'dispsize' =>  $dd['flat'][$column]['dispsize']
+                ,'type_id'  =>  $dd['flat'][$column]['type_id']
+                ,'description'=>$desc
+                ,'sortable'   =>true
+            );
+            #$grid->addColumn($dd['flat'][$column]);
+            $grid->addColumn($options);
+        }
+        $gridWidth=$grid->lastColumn();
+        
+        # Now set the left-padding to be 1/3 of remaining space
+        $remain = x6cssDefine('insidewidth') - $gridWidth;
+        $remain = intval($remain/3);
+        $top->hp['style'] = "padding-left: {$remain}px";
+        
+        # Also, let's set the width of the button bar
+        # now that we know what it is
+        $bb->hp['style'] = "width: {$gridWidth}px";
+        
+        # The data...
+        $ob='';
+        if($queuepos) $ob = " order by $queuepos"; 
+        $rows = SQL_AllRows("Select skey,$uisearch from $table_id $ob");
+        foreach($rows as $row) {
+            $grid->addRow($row['skey']);
+            foreach($aColumns as $column) {
+                $grid->addCell($row[$column]);
+            }
+        }
+        
+        # always at the end, render it
+        $top->render();
+    }    
+    # ===================================================================
+    # *******************************************************************
+    #
+    # Profile 1: "twosides"
+    #
+    # *******************************************************************
+    # ===================================================================
+    function profile_twosides() {
+        # Grab the data dictionary for this table
+        $dd       = $this->dd;
+        $table_id = $this->dd['table_id'];
+        
+        # Get the standard padding, we are going to double it
+        $pad0 = x6CSSDefine('pad0');
+        $heightRemain = x6cssDefine('insideheight');
+        
+        # Now put in your basic title
+        $div = new androHTMLTableController($table_id);
+        $div->addClass('fadein');
+        $div->ap['xCache']   = 'Y';  // results will be cached
+        $div->h('h1','<center>'.$dd['description'].'</center>');
+        $heightRemain -= x6cssHeight('h1');
+        $heightRemain -= ($pad0*2);
+        
+        # Create a two-sided layout by creating two boxes
+        # Left side is a grid with the entire table
+        $area0 = $div->h('div');
+        $area0->hp['style'] = "float: left; 
+            padding-left: {$pad0}px;
+            padding-right: {$pad0}px;";
+        $x6grid = $area0->addTabDiv($heightRemain,$table_id,false,true,false);
+        $x6grid->hp['x6profile'] = 'twosides';
+        $this->tabDivGeneric($x6grid,$this->dd);
+        
+        # Now put the data over there
+        $uisearch = $this->dd['projections']['_uisearch'];
+        $ob       = $this->dd['pks'];
+        $rows = SQL_AllRows("Select skey,$uisearch from $table_id $ob");
+        $aColumns = explode(',',$uisearch);
+        foreach($rows as $row) {
+            $x6grid->addRow($row['skey']);
+            foreach($aColumns as $column) {
+                $x6grid->addCell($row[$column]);
+            }
+        }
+        
+        # Calculate how much width is left
+        $wInner = x6CSSDefine('insidewidth');
+        $wInner-=$x6grid->width;
+        $wInner-=2;  // assume a border on the grid
+        $wInner-=2;  // assume a border on the right-side
+        $wInner-= ($pad0*6); // 3 times padding doubled
+        
+        $box2  = $div->h('div');
+        $box2->hp['style'] = "float: left; width: {$wInner}px;";
+        $box2->addDetail($table_id,true,$heightRemain-2);
+        
+        # Render it!  That's it!
+        $div->render();
+    }
+    
+    
     
     # ===================================================================
     # *******************************************************************
     #
-    # Profile 0: "conventional"
+    # Profile 2: "conventional"
     #
     # *******************************************************************
     # ===================================================================
@@ -485,169 +651,6 @@ class androX6 {
     }
 
     # ===================================================================
-    # *******************************************************************
-    #
-    # Profile 1: "twosides"
-    #
-    # *******************************************************************
-    # ===================================================================
-    function profile_twosides() {
-        # Grab the data dictionary for this table
-        $dd       = $this->dd;
-        $table_id = $this->dd['table_id'];
-        
-        # Get the standard padding, we are going to double it
-        $pad0 = x6CSSDefine('pad0');
-        $heightRemain = x6cssDefine('insideheight');
-        
-        # Now put in your basic title
-        $div = new androHTMLTableController($table_id);
-        $div->addClass('fadein');
-        $div->ap['xCache']   = 'Y';  // results will be cached
-        $div->h('h1','<center>'.$dd['description'].'</center>');
-        $heightRemain -= x6cssHeight('h1');
-        $heightRemain -= ($pad0*2);
-        
-        
-        # Create a two-sided layout by creating two boxes
-        # Left side is a grid plugin
-        $area0 = $div->h('div');
-        $area0->hp['style'] = "float: left; 
-            padding-left: {$pad0}px;
-            padding-right: {$pad0}px;";
-        include 'x6plugingrid.php';
-        $x6grid = new x6plugInGrid;
-        $x6grid->main($area0,$dd,0,$heightRemain);
-        
-        # Calculate how much width is left
-        $wInner = x6CSSDefine('insidewidth');
-        $wInner-=$x6grid->width;
-        $wInner-=2;  // assume a border on the grid
-        $wInner-=2;  // assume a border on the right-side
-        $wInner-= ($pad0*6); // 3 times padding doubled
-        
-        $box2  = $div->h('div');
-        $box2->hp['style'] = "float: left; 
-            width: {$wInner}px;
-            padding-left: {$pad0}px;
-            padding-right: {$pad0}px;";
-        $box2->hp['onkeydown'] = 'x6inputs.keyDown(event,this)';
-        #include 'x6plugindetailDisplay.php';
-        #$x6detail = new x6plugindetailDisplay;
-        #$x6detail->main($box2,$dd,$heightRemain-2);
-        
-        # Render it!  That's it!
-        $div->render();
-    }
-    # ===================================================================
-    # *******************************************************************
-    #
-    # Profile 2: "tabDiv"  editable!
-    #
-    # *******************************************************************
-    # ===================================================================
-    function profile_tabDiv() {
-        # these were provided by the code that instantiated
-        # and initialized the object.
-        $dd       = $this->dd;
-        $table_id = $this->dd['table_id'];
-        
-        # Scan through and look for a single queuepos column,
-        # it changes things considerably: the queuepos column
-        # is hidden and sorting is forced on that column.  It is
-        # also assigned automatically as the user creates rows.
-        $queuepos = '';
-        foreach($dd['flat'] as $colname=>$colinfo) {
-            if(strtolower($colinfo['automation_id'])=='queuepos') {
-                $queuepos = $colname;
-                break;
-            }
-        }
-        # Make sortable or not based on queuepos
-        $sortable = $queuepos=='';
-        
-        # Create the top level div
-        $top=new androHTMLTableController($table_id);
-        $top->addClass('fadein');
-        
-        $hremain = x6cssDefine('insideheight');
-        
-        # Get us the basic title
-        $top->h('h1',$dd['description']);
-        $hremain -= x6cssHeight('h1');
-        $bb = $top->addButtonBar($table_id);
-        $hremain -= x6cssHeight('div.x6buttonBar a.button');
-        $clearboth = $top->h('div');
-        $clearboth->hp['style'] = 'clear: both';
-        
-        # Work out a height by finding out inside height
-        # and subtracing line height a few times
-        $gridHeight = $hremain - x6cssHeight('h1');
-        $grid       = $top->addTabDiv($gridHeight,false,$sortable);
-        $grid->hp['x6table']      = $table_id;
-        $grid->hp['id']           = "tabDiv_$table_id";
-        $grid->editInPlace();
-        
-        # More features specific to this profile, these will 
-        # allow browseFetch not to have to figure this all out
-        # all over again.
-        $grid->ap['xGridHeight'] = $gridHeight;
-        $grid->ap['xSortable']   = $sortable ? "Y" : "N";
-        $grid->ap['xReturnAll']  = "Y";
-
-        # If queuepos set, set this flag        
-        if($queuepos) $grid->ap['xInsertAfter'] = 'Y';
-        
-        
-        # Now obtain the _uisearch columns and make one column each
-        $uisearch = $dd['projections']['_uisearch'];
-        $aColumns = explode(',',$uisearch);
-        foreach($aColumns as $idx=>$column) {
-            if($column==$queuepos) unset($aColumns[$idx]);
-        }
-        foreach($aColumns as $column) {
-            $desc = $dd['flat'][$column]['descshort'];
-            if($desc=='') {
-                $desc = $dd['flat'][$column]['description'];
-            }
-            $options=array(
-                'column_id' =>  $column
-                ,'dispsize' =>  $dd['flat'][$column]['dispsize']
-                ,'type_id'  =>  $dd['flat'][$column]['type_id']
-                ,'description'=>$desc
-                ,'sortable'   =>true
-            );
-            #$grid->addColumn($dd['flat'][$column]);
-            $grid->addColumn($options);
-        }
-        $gridWidth=$grid->lastColumn();
-        
-        # Now set the left-padding to be 1/3 of remaining space
-        $remain = x6cssDefine('insidewidth') - $gridWidth;
-        $remain = intval($remain/3);
-        $top->hp['style'] = "padding-left: {$remain}px";
-        
-        # Also, let's set the width of the button bar
-        # now that we know what it is
-        $bb->hp['style'] = "width: {$gridWidth}px";
-        
-        # The data...
-        $ob='';
-        if($queuepos) $ob = " order by $queuepos"; 
-        $rows = SQL_AllRows("Select skey,$uisearch from $table_id $ob");
-        foreach($rows as $row) {
-            $grid->addRow($row['skey']);
-            foreach($aColumns as $column) {
-                $grid->addCell($row[$column]);
-            }
-        }
-        
-        # always at the end, render it
-        $top->render();
-    }    
-    
-    
-    # ===================================================================
     # -------------------------------------------------------------------
     #
     # Simple Library routines
@@ -658,9 +661,9 @@ class androX6 {
     # cells to it for a browseFetch and then pluck out the tbody html
     function tabDivGeneric(&$grid,$dd,$tabPar='') {
         $table_id = $dd['table_id'];
-        $grid->hp['x6table']      = $table_id;
-        $grid->hp['id']           = "tabDiv_$table_id";
-        $grid->hp['xSortable']    = 'Y';
+        #$grid->hp['x6table']      = $table_id;
+        #$grid->hp['id']           = "tabDiv_$table_id";
+        #$grid->hp['xSortable']    = 'Y';
         
         # KFD 11/15/08 
         # Nifty trick to allow different columns when viewed as 
