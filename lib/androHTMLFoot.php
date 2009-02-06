731fdbcd753deb38aa4e9a39d94675ad124c999a
kendowns <kendowns@d2947088-3ffd-11de-917f-4137a9cc17b9>
2009-02-06 17:14:31 +0000

    Corrected sequence of output for final material at end of page.  The problem this fixes only appears when you use the (as yet) undocumented detail editing on 1:m screens.

--- lib/androHTMLFoot.php
+++ lib/androHTMLFoot.php
@@ -20,49 +20,6 @@ if(vgfGet('HTML_focus')<>'') {
     jqDocReady('$("#'.$f.'").focus()');
 }
 
-
-# =====================================================================
-#
-# Old-fashioned pre-x4 pre-jquery way to set focus.
-# Don't ever use this!  It will be deprecated
-# sooner or later.
-#
-# =====================================================================
-$jqdr = vgfGet('jqDocReady',array());
-if(count($jqdr)>0) {
-    ?>
-    <script type="text/javascript">
-    $(document).ready(function() {
-<?php echo implode("\n",$jqdr)?> 
-    });
-    </script>
-    <?php
-}
-/*  Original way pre 7/25/08, before we deprecated
-             elementadd()
-$jqueryDocumentReady = ElementImplode('jqueryDocumentReady');
-if($jqueryDocumentReady<>'') {
-    ?>
-    <script type="text/javascript">
-    $(document).ready(function() {
-    <?php echo $jqueryDocumentReady?>
-    });
-    </script>
-    <?php
-}
-*/
-
-if(configGet('deprecated','Y')=='Y') {
-    $scriptend = ElementImplode('scriptend');
-    if($scriptend<>'') {
-        ?>
-        <script type="text/javascript">
-        <?php echo $scriptend?>
-        </script>
-        <?php
-    }
-}
-
 # -------------------------------------------------------------
 # Put out some invisible divs that are used for modals, 
 # popups and so forth
@@ -100,25 +57,57 @@ if(configGet('deprecated','Y')=='Y') {
 ?>    
 </div>
 <?php
-    # KFD 12/31/08.  Restrict the query log to people who have
-    #                set the cookie and are in the 'debuggers' group
-    $debugging = inGroup('debugging');
-    $cookie    = arr($_COOKIE,'log_Server',0);
-    if($debugging && $cookie) {
-        echo( '<br /><div class="androQueryLog">' );
-        echo( '<div class="androQueryLogTitle">
-            <div style="float:left;height:20px;">Query Log</div><div style="float:right;height:20px;cursor:pointer;" onclick="showHide(\'androQueryLogItems\');">Show/Hide</div></div>' );
-        echo( '<div class="androQueryLogItems" id="androQueryLogItems">' );
-        foreach ( $GLOBALS['AG']['dbg']['sql'] as $key=>$line ) {
-            echo( '<div class="androQueryLogItem" style="width:auto;"><strong>Query:</strong> ' 
-                .'<div><pre style="max-width:100%;">'.$line['sql'].'</pre></div>
-                <div><strong>Execution time:</strong>' .number_format( $line['time'],4 ) .'</div>
-                <div onclick="showHide(\'stack-' .$key .'\');" style="cursor:pointer;">More Details</div>' );
-            echo( '<div style="display:none;" id="stack-' .$key .'">' );
-            echo "<strong>Execution Stack:</strong><pre>"
-                .$line['stack']."</pre></div>";     
-            echo "</div>";
-        }
-        echo( '</div></div>' );
+# KFD 12/31/08.  Restrict the query log to people who have
+#                set the cookie and are in the 'debuggers' group
+$debugging = inGroup('debugging');
+$cookie    = arr($_COOKIE,'log_Server',0);
+if($debugging && $cookie) {
+    echo( '<br /><div class="androQueryLog">' );
+    echo( '<div class="androQueryLogTitle">
+        <div style="float:left;height:20px;">Query Log</div><div style="float:right;height:20px;cursor:pointer;" onclick="showHide(\'androQueryLogItems\');">Show/Hide</div></div>' );
+    echo( '<div class="androQueryLogItems" id="androQueryLogItems">' );
+    foreach ( $GLOBALS['AG']['dbg']['sql'] as $key=>$line ) {
+        echo( '<div class="androQueryLogItem" style="width:auto;"><strong>Query:</strong> ' 
+            .'<div><pre style="max-width:100%;">'.$line['sql'].'</pre></div>
+            <div><strong>Execution time:</strong>' .number_format( $line['time'],4 ) .'</div>
+            <div onclick="showHide(\'stack-' .$key .'\');" style="cursor:pointer;">More Details</div>' );
+        echo( '<div style="display:none;" id="stack-' .$key .'">' );
+        echo "<strong>Execution Stack:</strong><pre>"
+            .$line['stack']."</pre></div>";     
+        echo "</div>";
     }
+    echo( '</div></div>' );
+}
+
+
+# =====================================================================
+#
+# Old-fashioned pre-x4 pre-jquery way to set focus.
+# Don't ever use this!  It will be deprecated
+# sooner or later.
+#
+# =====================================================================
+$jqdr = vgfGet('jqDocReady',array());
+if(count($jqdr)>0) {
+    ?>
+    <script type="text/javascript">
+    $(document).ready(function() {
+<?php echo implode("\n",$jqdr)?> 
+    });
+    </script>
+    <?php
+}
+
+if(configGet('deprecated','Y')=='Y') {
+    $scriptend = ElementImplode('scriptend');
+    if($scriptend<>'') {
+        ?>
+        <script type="text/javascript">
+        <?php echo $scriptend?>
+        </script>
+        <?php
+    }
+}
 ?>
+
+
