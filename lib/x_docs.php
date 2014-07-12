24ef6a6e73ea65054dc17a48745c4fd8a29821f2
Donald Organ <dorgan@donaldorgan.com>
2014-07-11 23:25:10 -0400

    fixing unterminated comment

--- lib/x_docs.php
+++ lib/x_docs.php
@@ -64,6 +64,5 @@ They click on something else, another page comes up.
 A page, therefore, is what goes back to the browser 
 
 Some programmers will think of a page as equal to a file on disk.
-
-
+*/
 ?>
